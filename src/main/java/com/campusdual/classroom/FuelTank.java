44db78edaeebe17435812d736af7d30c655565fa
Hugo Rodriguez Marcelino <hugorgzmar@gmail.com>
2024-09-30 10:25:05 +0200

    Encapsulacion

--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -2,13 +2,33 @@ package com.campusdual.classroom;
 
 public class FuelTank {
 
-  public int actualFuel = 10;
+  private int actualFuel = 10;
+
+
 
   public void showDetails() {
-    System.out.println("La capacidad actual es de " + this.actualFuel + " litros.");
+    int fuel = getActualFuel();
+    System.out.println("La capacidad actual es de " + fuel + " litros.");
   }
 
   public int getActualFuel() {
     return this.actualFuel;
   }
+
+
+  public void setActualFuel(int fuel){
+
+      if (fuel > 0) {
+          this.actualFuel = fuel;
+      } else {
+        System.out.println("Error: No se puede establecer un valor negativo");
+      }
+  }
+
+
+
 }
+
+
+
+
